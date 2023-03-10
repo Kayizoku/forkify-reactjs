d8f5f6bfb8b0e615c5df3be4cd0980ffcb202c60
Mohammad Tayyab Khalid <tayyabkhalid306@gmail.com>
2023-03-10 11:46:23 +0100

    Render content based on hash id

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,4 +1,7 @@
 import icons from 'url:../img/icons.svg';
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
+
 const recipeContainer = document.querySelector('.recipe');
 
 const timeout = function (s) {
@@ -23,10 +26,12 @@ const renderSpinner = function (parentEl) {
 
 const showRecipe = async function () {
   try {
+    const id = window.location.hash.slice(1);
+    console.log(id);
     // 1) Loading recipe
     renderSpinner(recipeContainer);
     const res = await fetch(
-      'https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886'
+      `https://forkify-api.herokuapp.com/api/v2/recipes/${id}`
     );
     const data = await res.json();
 
@@ -148,4 +153,4 @@ const showRecipe = async function () {
     alert(err);
   }
 };
-showRecipe();
+window.addEventListener('hashchange', showRecipe);
