a67d7d1cc16b354119e5ea5c9332544d6f0c67cc
Mohammad Tayyab Khalid <tayyabkhalid306@gmail.com>
2023-03-11 14:45:29 +0100

    Real world way to render errors messages

--- src/js/controller.js
+++ src/js/controller.js
@@ -7,14 +7,6 @@ import 'regenerator-runtime/runtime';
 
 const recipeContainer = document.querySelector('.recipe');
 
-const timeout = function (s) {
-  return new Promise(function (_, reject) {
-    setTimeout(function () {
-      reject(new Error(`Request took too long! Timeout after ${s} second`));
-    }, s * 1000);
-  });
-};
-
 const controlRecipes = async function () {
   try {
     const id = window.location.hash.slice(1);
@@ -29,12 +21,14 @@ const controlRecipes = async function () {
     // 2) Rendering recipe
     recipeView.render(model.state.recipe);
   } catch (err) {
-    alert(err);
+    recipeView.renderError();
   }
 };
-['hashchange', 'load'].forEach(ev =>
-  window.addEventListener(ev, controlRecipes)
-);
+
+const init = function () {
+  recipeView.addHandlerRender(controlRecipes);
+};
+init();
 
 // https://forkify-api.herokuapp.com/v2
 
